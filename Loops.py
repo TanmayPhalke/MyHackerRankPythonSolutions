d74636e2adc87473c32ec22e788f73ba93ef303d
TanmayPhalke <31072498+TanmayPhalke@users.noreply.github.com>
2020-06-27 16:04:35 +0530

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(raw_input())
+    if n < 20:
+        for i in range(0,n):
+            print(i*i)
+    else:
+        print("Enter the number less than 20. ")
