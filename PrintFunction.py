fac693bd70bf311ceb40c468858b538ab123a0bf
TanmayPhalke <31072498+TanmayPhalke@users.noreply.github.com>
2020-06-27 16:29:35 +0530

    Create PrintFunction.py

---
+++ PrintFunction.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    for i in range(1,n):
+        #end:  string appended after the last value, default a newline
+        print(i,end='')
+    print(n)
